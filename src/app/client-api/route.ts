d5be7b06b5f6e013f99c5d0d6c0ce03634ab4e28
Sumanta Kabiraj <sumanta@bexcodeservices.com>
2025-02-08 00:58:00 +0530

    add security definitions and metadata to client API specification

--- src/app/client-api/route.ts
+++ src/app/client-api/route.ts
@@ -6,6 +6,11 @@ import {
 const config: ApiReferenceOptions = {
   theme: "default",
   forceDarkModeState: "light",
+  hideDarkModeToggle: true,
+  metaData:{
+    title: "Ola MD Client API",
+    description: "Client API description",
+  },
   spec: {
     url: "/specs/client-api.yml",
   },
